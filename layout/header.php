518afc3a578d4774ab988a0e9dead5088b7128dd
ThomasBronola <89704625+ThomasBronola@users.noreply.github.com>
2021-09-15 01:50:48 +0800

    New: "Meet the Developers"

--- layout/header.php
+++ layout/header.php
@@ -14,7 +14,8 @@
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto text-uppercase">
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#home">HOME</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">TEAM</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">ABOUT</a></li>                    
                     <li class="nav-item"><a class="nav-link" data-toggle="modal" href="#las-modal">LOGIN</a></li>
                 </ul>
             </div>
