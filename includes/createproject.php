13ad975892f2d825d757e95380321246755b9987
alexissorianooo <alexis.soriano27@gmail.com>
2021-09-24 21:11:59 +0800

    Create project with additional activities COMPLETE

--- includes/createproject.php
+++ includes/createproject.php
@@ -35,46 +35,46 @@ if(isset($_POST['createButton'])){
     $project_input8 = $_POST['project_input8'];
 
 
-    if($counter!=0){ //checks if there is an additional activities
-        $project = 8;
-        for ($i=0; $i<5;$i++){
-            $project++;
+    // if($counter!=0){ //checks if there is an additional activities
+    $project = 8;
+    for ($i=0; $i<5;$i++){
+        $project++;
 
-            // echo $project_input["project_input".$project] = $_POST['additional_name'. $i+1]; WORKING!!
+        // echo $project_input["project_input".$project] = $_POST['additional_name'. $i+1]; WORKING!!
 
-            if(!empty($_POST['additional_name'. $i+1])){
-                $project_input["project_input".$project] = $_POST['additional_name'. $i+1];
-            } else{
-                $project_input["project_input".$project] = "empty";
-            }
-            extract($project_input);
-
-
+        if(!empty($_POST['additional_name'. $i+1])){
+            $project_input["project_input".$project] = $_POST['additional_name'. $i+1];
+        } else{
+            $project_input["project_input".$project] = "empty";
         }
-        echo $project_input9 .'<br>';
-        echo $project_input10 .'<br>';
-        echo $project_input11 .'<br>';
-        echo $project_input12 .'<br>';
-        echo $project_input13 .'<br>';
-        //PUT NORMAL ACTIVITIES HERE WITH ADDITIONAL?
-    }else{
-        echo 'there is no additional activities';
-        //PUT NORMAL ACTIVITIES HERE?
+        extract($project_input);
+
+
     }
+    // echo $project_input9 .'<br>';
+    // echo $project_input10 .'<br>';
+    // echo $project_input11 .'<br>';
+    // echo $project_input12 .'<br>';
+    // echo $project_input13 .'<br>';
+        //PUT NORMAL ACTIVITIES HERE WITH ADDITIONAL?
+    // }else{
+    //     echo 'there is no additional activities';
+    //     //PUT NORMAL ACTIVITIES HERE?
+    // }
     
     
     
 
-    // require_once 'db.php';
-    // require_once 'functions.php';
+    require_once 'db.php';
+    require_once 'functions.php';
 
-    // if(emptyInputcreate($project_name, $project_startdate, $project_deadline, $project_architect, $project_pmSELECT, $project_input1, $project_input2, $project_input3, $project_input4, $project_input5, $project_input6, $project_input7, $project_input8) !==false){
-    //     header("location: ../users/Projects/project_arch.php?error=emptyinput");
-    //     exit();
-    // } 
+    if(emptyInputcreate($project_name, $project_startdate, $project_deadline, $project_architect, $project_pmSELECT, $project_input1, $project_input2, $project_input3, $project_input4, $project_input5, $project_input6, $project_input7, $project_input8) !==false){
+        header("location: ../users/Projects/project_arch.php?error=emptyinput");
+        exit();
+    } 
    
 
-    // createProject($conn, $project_name, $project_startdate, $project_deadline, $project_architect, $project_pmSELECT, $project_input1, $project_input2, $project_input3, $project_input4, $project_input5, $project_input6, $project_input7, $project_input8);
+    createProject($conn, $project_name, $project_startdate, $project_deadline, $project_architect, $project_pmSELECT, $project_input1, $project_input2, $project_input3, $project_input4, $project_input5, $project_input6, $project_input7, $project_input8, $project_input9, $project_input10, $project_input11, $project_input12, $project_input13);
     
 
 
