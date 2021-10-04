6fe50ee96ddf197c74b5dcb2e0dd4d805578567a
alexissorianooo <alexis.soriano27@gmail.com>
2021-10-04 16:47:47 +0800

    template for completed project for PM

---
+++ users/Projects/PM_viewproject.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>view completed project PM</title>
+    <link rel="stylesheet" href="assetsFORPM_viewproject/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assetsFORPM_viewproject/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assetsFORPM_viewproject/css/styles.css">
+    <link rel="stylesheet" href="assetsFORPM_viewproject/css/Table-With-Search-1.css">
+    <link rel="stylesheet" href="assetsFORPM_viewproject/css/Table-With-Search.css">
+</head>
+
+<body>
+    <div class="col-md-12 search-table-col">
+        <div class="form-group pull-right col-lg-4"><input type="text" class="search form-control" placeholder="Search by typing here.."></div><span class="counter pull-right"></span>
+        <div class="table-responsive table-bordered table table-hover table-bordered results">
+            <table class="table table-bordered table-hover">
+                <thead class="bill-header cs">
+                    <tr>
+                        <th style="max-width: 12.5%;">Project ID</th>
+                        <th id="trs-hd" class="col-lg-1" style="max-width: 12.5%;">Project Name</th>
+                        <th id="trs-hd" class="col-lg-2" style="max-width: 12.5%;">Architect</th>
+                        <th id="trs-hd" class="col-lg-3" style="max-width: 12.5%;">Foreman</th>
+                        <th id="trs-hd" class="col-lg-2" style="max-width: 12.5%;">Startdate</th>
+                        <th id="trs-hd" class="col-lg-2" style="max-width: 12.5%;">Deadline</th>
+                        <th style="max-width: 12.5%;">Date Completed</th>
+                        <th id="trs-hd" class="col-lg-2" style="max-width: 12.5%;">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <button class="btn btn-success" style="margin-left: 5px;background: var(--warning);" type="submit">
+                                <i class="fa fa-folder-open-o" style="font-size: 15px;">
+                                </i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="assetsFORPM_viewproject/js/jquery.min.js"></script>
+    <script src="assetsFORPM_viewproject/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assetsFORPM_viewproject/js/Table-With-Search.js"></script>
+</body>
+
+</html>
