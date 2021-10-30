8f85374b8e3f1350583f195c94c43eb0f2e57482
LaczFeli <90681326+LaczFeli@users.noreply.github.com>
2021-10-30 15:52:00 +0800

    Update landing-page.php

--- landing-page.php
+++ landing-page.php
@@ -175,7 +175,7 @@
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2 class="text-uppercase">About</h2>
-                    <h3 class="text-muted section-subheading">About Construc-IT</h3>
+                    <h3 class="text-muted section-subheading">Construc-IT Features</h3>
                 </div> 
             </div>            
             <div class="row">
